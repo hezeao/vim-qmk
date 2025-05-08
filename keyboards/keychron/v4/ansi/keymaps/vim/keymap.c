dc8ea2c4673d2460ae91d846198d37e04509d05d
hezeao <hezeao@foxmail.com>
2025-05-08 17:28:04 +0800

    feat: reconstructed the keyboard layout

--- keyboards/keychron/v4/ansi/keymaps/vim/keymap.c
+++ keyboards/keychron/v4/ansi/keymaps/vim/keymap.c
@@ -21,9 +21,9 @@
 enum layers {
     MAC_BASE,
     WIN_BASE,
-    _N1,
-    _V1,
-    _FN3
+    NORMAL,
+    VISUAL,
+    REPLACE
 };
 
 #define KC_TASK LGUI(KC_TAB)
@@ -35,30 +35,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,     KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,
         KC_ESC,  KC_A,     KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,            KC_ENT,
         KC_LSFT,           KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH,            KC_RSFT,
-        KC_LCTL, KC_LOPT,  KC_LCMD,                            KC_SPC,                             KC_RCMD,  TG(_N1),  TG(_V1), KC_RCTL),
+        XXXXXXX, KC_LCTL,  KC_LCMD,                            KC_SPC,                             KC_LOPT,  KC_RCMD,  KC_RCTL,  XXXXXXX),
 
     [WIN_BASE] = LAYOUT_60_ansi(
         KC_GRV,  KC_1,     KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,
         KC_TAB,  KC_Q,     KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,
         KC_ESC,  KC_A,     KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,            KC_ENT,
         KC_LSFT,           KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH,            KC_RSFT,
-        KC_LCTL, KC_LWIN,  KC_LALT,                            KC_SPC,                             KC_RALT,  TG(_N1),  TG(_V1), KC_RCTL),
+        XXXXXXX, KC_LCTL,  KC_LALT,                            KC_SPC,                             KC_LWIN,  KC_RALT,  KC_RCTL,  XXXXXXX),
 
-    [_N1] = LAYOUT_60_ansi(
+    [NORMAL] = LAYOUT_60_ansi(
         XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-        XXXXXXX, XXXXXXX,  XXXXXXX, C(KC_RGHT), KC_F2,   XXXXXXX, C(KC_C), C(KC_Z), KC_I,    XXXXXXX, C(KC_V),  XXXXXXX,  XXXXXXX,  XXXXXXX,
+        KC_TAB,  XXXXXXX,  XXXXXXX, C(KC_RGHT), KC_F2,   XXXXXXX, C(KC_C), C(KC_Z), KC_I,    XXXXXXX, C(KC_V),  XXXXXXX,  XXXXXXX,  XXXXXXX,
         KC_ESC,  XXXXXXX,  XXXXXXX, KC_DEL,  C(KC_F), KC_HOME, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX,  XXXXXXX,            KC_ENT,
-        XXXXXXX,           XXXXXXX, C(KC_X), XXXXXXX, KC_V,    C(KC_LEFT), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,            XXXXXXX,
-        XXXXXXX, KC_LWIN,  XXXXXXX,                            KC_SPC,                             XXXXXXX,  UG_TOGG,  QK_BOOT,  XXXXXXX),
+        KC_LSFT,           XXXXXXX, C(KC_X), XXXXXXX, KC_V,    C(KC_LEFT), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,            KC_RSFT,
+        XXXXXXX, KC_LCTL,  KC_LALT,                            KC_SPC,                             KC_LWIN,  KC_RALT,  KC_RCTL,  XXXXXXX),
 
-    [_V1] = LAYOUT_60_ansi(
+    [VISUAL] = LAYOUT_60_ansi(
         XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
         XXXXXXX, XXXXXXX,  XXXXXXX, S(C(KC_RGHT)), KC_F2,   XXXXXXX, C(KC_C), C(KC_Z), KC_I,    XXXXXXX, C(KC_V),  XXXXXXX,  XXXXXXX,  XXXXXXX,
         KC_ESC,  XXXXXXX,  XXXXXXX, KC_DEL,  C(KC_F), KC_HOME, S(KC_LEFT), S(KC_DOWN), S(KC_UP),   S(KC_RGHT), XXXXXXX,  XXXXXXX,            KC_ENT,
         XXXXXXX,           XXXXXXX, C(KC_X), XXXXXXX, XXXXXXX, S(C(KC_LEFT)), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,            XXXXXXX,
-        XXXXXXX, KC_LWIN,  XXXXXXX,                            XXXXXXX,                            XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX),
+        XXXXXXX, KC_LCTL,  XXXXXXX,                            XXXXXXX,                            UG_TOGG,  QK_BOOT,  XXXXXXX,  XXXXXXX),
 
-    [_FN3] = LAYOUT_60_ansi(
+    [REPLACE] = LAYOUT_60_ansi(
         XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
         XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
         XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,            XXXXXXX,
@@ -94,10 +94,10 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
 }
 
 // Suppressed_mods that are still held down will be re-added to the keyboard report.
-const key_override_t normal_override = {.trigger_mods      = MOD_BIT(KC_LGUI),                                 //
+const key_override_t normal_override = {.trigger_mods      = MOD_BIT(KC_LCTL),                                 //
                                         .layers            = ~(1 << 2),                                        //
                                         .options           = ko_option_no_unregister_on_other_key_down,        //
-                                        .negative_mod_mask = (uint8_t) ~(MOD_BIT(KC_LGUI)),                    //
+                                        .negative_mod_mask = (uint8_t) ~(MOD_BIT(KC_LCTL)),                    //
                                         .custom_action     = set_mode,                                         //
                                         .context           = (void *)2,                                        //
                                         .trigger           = KC_ESC,                                           //
